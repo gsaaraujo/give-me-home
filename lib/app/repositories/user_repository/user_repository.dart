e6e747120d3213390167ccf467a0badc55d49cf8
Gabriel Araujo <gabriel.houth@gmail.com>
2021-09-15 09:55:35 -0300

    feat: add user repository

---
+++ lib/app/repositories/user_repository/user_repository.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:give_me_home/app/repositories/user_repository/user_repository_interface.dart';
+
+class UserRepository implements IUserRepositoryInterface {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+
+  UserRepository(this.firestore, this.auth);
+
+  @override
+  Future<void> createUserIfNotExists() async {
+    final users = firestore.collection('users');
+    final currentUser = auth.currentUser;
+
+    if (currentUser != null) {
+      users.doc(currentUser.uid).set({
+        'name': currentUser.displayName,
+        'photoURL': currentUser.photoURL,
+      });
+    }
+  }
+}
